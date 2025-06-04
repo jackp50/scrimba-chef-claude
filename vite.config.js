62e5e5950a8ee233158a3e72e0c03968ebecd1c6
jackp50 <jpearne9@gmail.com>
2025-06-03 19:02:33 -0600

    new api key process

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/scrimba-chef-claude",
-  define: {
-    'import.meta.env.VITE_API_KEY': '""'
-  }
+  base: "/scrimba-chef-claude"
 })
