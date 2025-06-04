e125ffbc3e93552c35f5d6d400ef2de40dc5c90e
jackp50 <jpearne9@gmail.com>
2025-06-03 20:54:54 -0600

    fixed render

--- src/ai.js
+++ src/ai.js
@@ -1,5 +1,5 @@
 export async function getRecipeFromChefClaude(ingredientsArr) {
-    const response = await fetch("https://scrimba-chef-claude.onrender.com", {
+    const response = await fetch("https://scrimba-chef-claude.onrender.com/get-recipe", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ingredients: ingredientsArr }),
