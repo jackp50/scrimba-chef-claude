083b82451b6d7da8c120f9796be4ee70764157c2
jackp50 <jpearne9@gmail.com>
2025-06-03 20:14:43 -0600

    added render

--- src/ai.js
+++ src/ai.js
@@ -1,5 +1,5 @@
 export async function getRecipeFromChefClaude(ingredientsArr) {
-    const response = await fetch("http://localhost:3001/get-recipe", {
+    const response = await fetch("https://scrimba-chef-claude.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ingredients: ingredientsArr }),
